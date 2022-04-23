bc59fb4e4cd6eee547fa3737af1085d4f0913a54
MugiSus <MugiSus@users.noreply.github.com>
2022-04-23 10:44:49 +0000

    deploy: a6757301ed1ef0d81bedd18917a27d9510276e1c

---
+++ _next/static/FPY853Vs_SdsQ9EMus9He/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/929-ee6c2213bd0baaf1.js","static/css/390d1d15eda31618.css","static/chunks/pages/index-252fcce6999458b8.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/_QF0YVw9D6QYCz9Mll_h0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/929-ee6c2213bd0baaf1.js","static/css/1e827308f083e642.css","static/chunks/pages/index-0344e9b133d00151.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
