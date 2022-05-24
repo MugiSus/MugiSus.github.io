621c22e38e7f18cf63a110fd07425fd4577b2580
MugiSus <MugiSus@users.noreply.github.com>
2022-05-24 19:04:37 +0000

    deploy: 0076f0235348bec227ced95d7e1d67686434286a

--- _next/static/CqPNDtZl1JVN9j9rZXF9C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/589-23188fb655bc37d6.js","static/css/3b4d1496b73846e7.css","static/chunks/pages/index-7b17ce95fe80d205.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
