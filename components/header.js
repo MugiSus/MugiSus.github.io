2ef228e6798b4afe5df6706a1ff8e41b16486129
MugiSus <mmgiss256@gmail.com>
2022-06-14 07:43:24 +0900

    sidemenu commit

--- components/header.js
+++ components/header.js
@@ -1,15 +1,26 @@
+import Link from 'next/link';
 import styles from '../styles/Header.module.scss'
 
 const Header = () => (
     <div>
-        <header className={styles.container}>
+        <header className={styles.headerContainer}>
             <div className={styles.background} />
-            <span className={`material-icons-outlined ${styles.icons} ${styles.strokedText}`} data-text="menu">menu</span>
+            <label htmlFor={styles.checkboxSidemenu} className={`material-icons-outlined ${styles.icons} ${styles.strokedText}`} data-text="menu">menu</label>
             <div className={`${styles.title} ${styles.strokedText}`} data-text="MugiSus.com">MugiSus.com</div>
             <span className={`material-icons-outlined ${styles.icons} ${styles.strokedText}`} data-text="share">share</span>
         </header>
-
+        <input type="checkbox" className={styles.checkboxSidemenu} id={styles.checkboxSidemenu} />
         <div className={styles.placeholder} />
+        <label htmlFor={styles.checkboxSidemenu} className={styles.sidemenuBackground} />
+        <div className={styles.sidemenu}>
+            <div className={styles.background} />
+            <label htmlFor={styles.checkboxSidemenu} className={`material-icons-outlined ${styles.closeArrow} ${styles.strokedText}`} data-text="arrow_back">arrow_back</label>
+            <h2 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="MugiSus.com">MugiSus.com</h2>
+            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Creations">Creations</h3>
+            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Articles">Articles</h3>
+            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="MugiSus">MugiSus</h3>
+            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Careers">Careers</h3>
+        </div>
     </div>
 )
 
