71b01c072cfcf5f13f4c3c1c08fd33a0b1724e72
MugiSus <mmgiss256@gmail.com>
2022-06-14 06:27:04 +0900

    bring back header icon (does not work yet)

--- components/header.js
+++ components/header.js
@@ -4,9 +4,9 @@ const Header = () => (
     <div>
         <header className={styles.container}>
             <div className={styles.background} />
-            {/* <span className={`material-icons-outlined ${styles.icons}`}>menu</span> */}
-            <div className={styles.title}>MugiSus.com</div>
-            {/* <span className={`material-icons-outlined ${styles.icons}`}>share</span> */}
+            <span className={`material-icons-outlined ${styles.icons} ${styles.strokedText}`} data-text="menu">menu</span>
+            <div className={`${styles.title} ${styles.strokedText}`} data-text="MugiSus.com">MugiSus.com</div>
+            <span className={`material-icons-outlined ${styles.icons} ${styles.strokedText}`} data-text="share">share</span>
         </header>
 
         <div className={styles.placeholder} />
