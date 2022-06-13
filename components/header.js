9ff36ee328633ec92718eeca87d46387937e7734
MugiSus <mmgiss256@gmail.com>
2022-06-14 08:07:51 +0900

    commit

--- components/header.js
+++ components/header.js
@@ -15,11 +15,27 @@ const Header = () => (
         <div className={styles.sidemenu}>
             <div className={styles.background} />
             <label htmlFor={styles.checkboxSidemenu} className={`material-icons-outlined ${styles.closeArrow} ${styles.strokedText}`} data-text="arrow_back">arrow_back</label>
-            <h2 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="MugiSus.com">MugiSus.com</h2>
-            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Creations">Creations</h3>
-            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Articles">Articles</h3>
-            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="MugiSus">MugiSus</h3>
-            <h3 className={`${styles.sidemenuTitle} ${styles.strokedText}`} data-text="Careers">Careers</h3>
+            <Link href={""}>
+                <h2 className={styles.strokedText} data-text="MugiSus.com">MugiSus.com</h2>
+            </Link>
+            <Link href={"#creations"}>
+                <h3 className={styles.strokedText} data-text="Creations">Creations</h3>
+            </Link>
+            <Link href={"#articles"}>
+                <h3 className={styles.strokedText} data-text="Articles">Articles</h3>
+            </Link>
+            <Link href={"#mugisus"}>
+                <h3 className={styles.strokedText} data-text="MugiSus">MugiSus</h3>
+            </Link>
+            <Link href={"#careers"}>
+                <h3 className={styles.strokedText} data-text="Careers">Careers</h3>
+            </Link>
+            <Link href={"https://twitter.com/MugiSus"}>
+                <img className={styles.sidemenuLinkImage} src="/link-twitter.svg" alt="GitHub" />
+            </Link>
+            <Link href={"https://github.com/MugiSus"}>
+                <img className={styles.sidemenuLinkImage} src="/link-github.svg" alt="GitHub" />
+            </Link>
         </div>
     </div>
 )
