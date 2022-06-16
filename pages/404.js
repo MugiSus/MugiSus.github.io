8c17b3807b429e9092c605ed08e0f94d60a39a1c
Masayuki Minato <42643211+MugiSus@users.noreply.github.com>
2022-06-16 16:13:00 +0900

    fix

--- pages/404.js
+++ pages/404.js
@@ -3,3 +3,5 @@ import ErrorMessage from "../components/errorMessage.js"
 const ErrorPage = () => (
     <ErrorMessage statusCode={404} title={"Not Found"} message={"This page could not be found."} />
 )
+
+export default ErrorPage;
