ea3a048df29ab7b3083eb08c769f39b05236f78b
MugiSus <mmgiss256@gmail.com>
2022-06-17 16:09:15 +0900

    update error message

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import ErrorMessage from "../components/errorMessage.js"
 
 const ErrorPage = () => (
-    <ErrorMessage statusCode={404} title={"Not Found"} message={"The page you were looking for does not exist."} />
+    <ErrorMessage statusCode={404} title={"Not Found"} message={"This page could not be found."} />
 )
 
 export default ErrorPage;
--- pages/418.js
+++ pages/418.js
@@ -1,7 +1,7 @@
 import ErrorMessage from "../components/errorMessage.js"
 
 const ErrorPage = () => (
-    <ErrorMessage statusCode={418} title={"I'm a teapot"} message={"The thing you were trying to brew coffee is a teapot."} />
+    <ErrorMessage statusCode={418} title={"I'm a teapot"} message={"You were trying to brew coffee is a teapot."} />
 )
 
 export default ErrorPage;
