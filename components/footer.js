792af36da892ae30fc01328a426f785d95273c2f
MugiSus <mmgiss256@gmail.com>
2022-10-29 23:32:07 +0900

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.scss'
 const Footer = () => (
     <div>
         <footer className={styles.container}>
-            <div className={styles.copyright}>copyright ©︎ 2022 MugiSus</div>
+            <div className={styles.copyright}>copyright ©︎ {new Date().getFullYear()} MugiSus</div>
         </footer>
     </div>
 )
