470c8fff9e96e2d373a5a4a2fc21893fa2ec5a9a
MugiSus <mmgiss256@gmail.com>
2023-01-17 01:00:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,5 +11,14 @@ module.exports = {
 		)
 		return config
 	},
+	async redirects() {
+		return [
+			{
+				source: '/i',
+				destination: 'https://virtualcard-mugisus.vercel.app',
+				permanent: true,
+			},
+		]
+	},
     trailingSlash: true,
 }
