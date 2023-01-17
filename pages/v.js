2b0eb16fcfa1ee71256c07ea223ee323dba29e48
MugiSus <mmgiss256@gmail.com>
2023-01-18 01:30:54 +0900

    disabled redirecting and applied iframe

--- pages/v.js
+++ pages/v.js
@@ -1,14 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useRouter } from 'next/router'
 import { useEffect } from 'react';
+import styles from '../styles/VirtualCardIframe.module.scss'
 
-export default function Redirector() {
+export default function VirtualCardIframe() {
     const router = useRouter();
     
-    useEffect(() => {
-        const cardId = Object.keys(router.query)?.[0];
-        if (cardId) router.push(`../v-card/?id=${cardId}`);
-    }, [router.isReady]);
+    // useEffect(() => {
+    //     const cardId = Object.keys(router.query)?.[0];
+    //     if (cardId) router.push(`../v-card/?id=${cardId}`);
+    // }, [router.isReady]);
     
-    return null;
+    return (
+        <iframe src="https://www.mugisus.com/v-card?id=22aq" className={styles.frame} />
+    );
 }
