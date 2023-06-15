de293c8412f819f4fa2cc090a9980e8b59d68ac2
MugiSus <mmgiss256@gmail.com>
2023-06-15 14:56:44 +0900

    Update v.js

--- pages/v.js
+++ pages/v.js
@@ -6,18 +6,19 @@ import styles from '../styles/VirtualCardIframe.module.scss'
 
 export default function VirtualCardIframe() {
     const router = useRouter();
-    
-    // useEffect(() => {
-    //     const cardId = Object.keys(router.query)?.[0];
-    //     if (cardId) router.push(`../v-card/?id=${cardId}`);
-    // }, [router.isReady]);
+
+    const cardId = Object.keys(router.query)?.[0] ?? "none";
+    console.log(cardId)
     
     return (
         <div className={styles.container}>
             <Head>
                 <title>Virtual Card</title>
             </Head>
-            <iframe src="https://www.mugisus.com/v-card?id=22aq&embed=true" className={styles.frame} />
+            <iframe
+                src={`https://v-card-gilt.vercel.app/?id=${cardId}&embed=true`}
+                className={styles.frame}
+            />
         </div>
     );
 }
